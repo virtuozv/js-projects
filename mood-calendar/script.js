b4f4b91c07670568c6822f22357506a527011385
Pavel Strutinsky <pvorobiev@mmtr.ru>
2022-05-04 11:21:34 +0300

    added calendar (non-completed)

---
+++ mood-calendar/script.js
@@ -0,0 +1,20 @@
+const currentYear = 2022;
+const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+];
+
+const colors = ['#2d6b5f', '#72e3a6', '#dff4c7', '#edbf98', '#ea3d36'];
+const defaultColor = '#888';
+let activeColor = '';
