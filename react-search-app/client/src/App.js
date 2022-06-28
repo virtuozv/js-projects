012bb4579419bd53df01165966ee80e8072f7a81
Pavel Strutinsky <pvorobiev@mmtr.ru>
2022-06-28 15:43:46 +0300

    obs react panel search

---
+++ react-search-app/client/src/App.js
@@ -0,0 +1,11 @@
+import "./app.css";
+
+function App() {
+  return (
+    <div className="app">
+      <input type="text" placeholder="Search..." className="search" />
+    </div>
+  );
+}
+
+export default App;
